92effdbe3f4fdbb4afb43b307454f86ece743e87
Alex sheffcer <a.sheffcer@gmail.com>
2021-01-14 16:34:40 +0200

    make AppButton

---
+++ src/AppButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <button class=btn></button>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+    text: String,
+    color: {
+      type: String,
+      default: '',
+      validator (value) {
+        return ['', 'primary', 'danger'].includes(value)
+      }
+    }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
